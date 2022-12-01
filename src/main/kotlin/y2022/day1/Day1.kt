7a1389c131719081a58cbfa6b90a315f89a406e4
Lech Piechota <lech.piechota@ocado.com>
2022-12-01 12:51:57 +0100

    solutions for day 1

---
+++ src/main/kotlin/y2022/day1/Day1.kt
@@ -0,0 +1,34 @@
+package y2022.day1
+
+import java.io.File
+
+
+private fun getCaloriesMap(input: List<String>): Map<Int, Int> {
+    var elfId = 0
+    val caloriesMap = mutableMapOf<Int, Int>()
+    input.forEach { line ->
+        if(line.isBlank()) {
+            elfId += 1
+        } else {
+            caloriesMap[elfId] = line.toInt() + caloriesMap.getOrDefault(elfId, 0)
+        }
+    }
+    return caloriesMap
+}
+
+fun findMaxCalories(input: List<String>) = getCaloriesMap(input).values.max()
+
+/**
+ * sorting has O(n*log(n)) time complexity
+ * it could be done faster by iterating three times on map, each next time with maximum element removed
+ */
+fun findTopThreeMaxCalories(input: List<String>): Int = getCaloriesMap(input).values.sortedDescending().take(3).sum()
+
+
+
+fun main() {
+//    val input = File("src/main/resources/y2022/day1/testInput.txt").useLines { it.toList() }
+    val input = File("src/main/resources/y2022/day1/input.txt").useLines { it.toList() }
+    println(findMaxCalories(input))
+    println(findTopThreeMaxCalories(input))
+}
