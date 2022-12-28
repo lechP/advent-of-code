4fe8b370ad756d1a422e3d5765d21f0e39663d78
Lech Piechota <lech.piechota@ocado.com>
2022-12-28 18:46:10 +0100

    Day21 task2 algorithmic solution

--- src/main/kotlin/y2022/day21/Day21.kt
+++ src/main/kotlin/y2022/day21/Day21.kt
@@ -26,24 +26,46 @@ private fun findResult(monkey: String, map: Map<String, String>): Long {
 
 private fun String.isLong() = toLongOrNull() != null
 
-fun task2(input: List<String>) {
+fun task2(input: List<String>): Long {
     val monkeyMap = input.associate { line -> line.split(": ").let { it[0] to it[1] } }
-    // very primitive manual binary search
-    // my number decreases final result
-    // first candidate: 3412650897410 - too high
-    // but it turns out that couple of numbers gives the same result:
+
+    var lo = 0L
+    var hi = Int.MAX_VALUE.toLong()
+    // increase hi until it gets on the other side of zero
+    while (sidesDiff(monkeyMap, lo).sign() == sidesDiff(monkeyMap, hi).sign()) hi *= 2
+    // find candidate by binary search
+    var mid = (hi + lo) / 2
+    while (sidesDiff(monkeyMap, mid) != 0L) {
+        if (sidesDiff(monkeyMap, lo).sign() == sidesDiff(monkeyMap, mid).sign()) {
+            lo = mid
+        } else {
+            hi = mid
+        }
+        mid = (hi + lo) / 2
+    }
+
+    // it turns out that couple of numbers gives the same result:
     // 0,1: ..8355 2-8 ..8025 9-15 ..7690 16- ..7385 // 330 , 335 , 305
-    // so I find min which still gives equality
-    val map2 = replaceHumanValue(monkeyMap, 3412650897405)
-    val sides = monkeyMap["root"]!!.split(" + ")
-    println(findResult(sides[0], map2))
-    println(findResult(sides[1], map2))
-    println(findResult(sides[0], map2)==findResult(sides[1], map2))
-    println(findResult(sides[0], map2)-findResult(sides[1], map2))
+    // so I find minimal value which still gives equality
+    while (sidesDiff(monkeyMap, mid - 1) == 0L) mid -= 1
+
+    return mid
+}
+
+fun sidesDiff(map: Map<String, String>, candidate: Long): Long {
+    val replacedMap = replaceHumanValue(map, candidate)
+    val sides = replacedMap["root"]!!.split(" + ")
+    return findResult(sides[0], replacedMap) - findResult(sides[1], replacedMap)
+}
+
+fun Long.sign() = when {
+    this < 0 -> -1
+    this > 0 -> 1
+    else -> 0
 }
 
 private fun replaceHumanValue(map: Map<String, String>, humanValue: Long) =
-    map.mapValues { (key, value) -> if(key=="humn") humanValue.toString() else value }
+    map.mapValues { (key, value) -> if (key == "humn") humanValue.toString() else value }
 
 
-fun main() = printSolutions(21, 2022, {  }, { input -> task2(input) })
+fun main() = printSolutions(21, 2022, { input -> task1(input) }, { input -> task2(input) })
